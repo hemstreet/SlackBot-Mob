05f48032558aa0ea5284391f69fc5d4161ad73f5
Hemstreet <jonhemstreet@gmail.com>
2016-02-14 12:40:41 -0700

    updated for message priority

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var config = require('./config/config'),
     actions = require('./config/actions'),
     SlackBot = require('slackbots'),
@@ -6,28 +8,35 @@ var config = require('./config/config'),
     BotActions = require('./lib/Actions'),
     _ = require('underscore');
 
-console.log(argv);
 // create a bot
 var bot = new SlackBot({
     token: argv.apikey || config.apiKey, // Add a bot https://my.slack.com/services/new/bot and put the token
     name: argv.name || config.name || 'Chuck Norris'
 });
 
-bot.on('start', function() {
+bot.on('start', function () {
     utils.init(bot, config);
 
     var botId = utils.getBotId();
     var botActions = new BotActions(bot);
 
-    bot.on('message', function(data) {
+    bot.on('message', function (data) {
 
+        var actionTaken = false;
         // Is a message and is it from someone other than the bot itself?
-        if(data.type == 'message' && botId !== data.user) {
-
-            _.each(actions, function(value, key) {
-                if(utils.messageContains(data.text, value.keywords)) {
-
-                    botActions[key](value.channels);
+        if (data.type == 'message' && botId !== data.user) {
+            _.each(actions, function (value, key) {
+                if(!actionTaken) {
+                    if (utils.messageContains(data.text, value.keywords)) {
+                        actionTaken = true;
+                        botActions[key]({
+                            channels: value.channels
+                        });
+                    }
+                }
+                else
+                {
+                    // Action has already been taken on the message
                 }
             });
         }
